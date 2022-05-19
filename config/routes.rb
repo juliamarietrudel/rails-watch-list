26ac0e9ebd93136cc2bc81f7785af8fd907313f6
juliamarietrudel <juliamarietrudel96@gmail.com>
2022-05-19 15:20:42 +0200

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, except: %i[create new]
+  get '/lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new'
+  post '/lists', to: 'lists#create'
+  get '/lists/:id', to: 'lists#show'
 end
